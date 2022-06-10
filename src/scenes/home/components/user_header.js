86a3c4580fa1c40f03d3eacc9e41ea1f4e3cbf73
gacneves <gabriel.angelo1008@gmail.com>
2022-06-10 18:32:49 -0300

    Added user info

--- src/scenes/home/components/user_header.js
+++ src/scenes/home/components/user_header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import UserIcon from "./user_icon";
 
 export default function UserHeader(props) {
@@ -8,10 +10,16 @@ export default function UserHeader(props) {
       <View style={{flex:2, justifyContent: 'center', alignItems: 'center'}}>
         <UserIcon />
       </View>
-      <View style={{flex:3}}>
-        <Text>User Name</Text>
+      <View style={{flex:4, paddingTop: 20}}>
+        <Text style={styles.user_info}>Guguinha dos Santos</Text>
+        <View style={{flex: 1, flexDirection: 'row', paddingTop: 5}}>
+          <View style={{paddingTop: 3}}>
+            <FontAwesomeIcon icon={faStar} color={'#DEB841'} size={28} />
+          </View>
+          <Text style={[styles.user_info, {marginTop: 5}]}> 4.87</Text>
+        </View>
       </View>
-      <View style={{flex:2}}>
+      <View style={{flex:1}}>
         <Text>Chat</Text>
       </View>
     </SafeAreaView>
@@ -24,4 +32,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     backgroundColor: '#6D6A75'
   },
+  user_info: {
+    color: '#E6E6E6',
+    fontWeight: 'bold',
+    fontSize: 18
+  }
 });
