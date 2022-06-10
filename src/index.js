741b2a2bca8171beea1664d594bf70c881149bb9
gacneves <gabriel.angelo1008@gmail.com>
2022-06-09 23:11:33 -0300

    Added initial Home Screen

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
-import { Text, View } from "react-native";
+import HomeScreen from "./scenes/home";
 
 export default function App() {
   return (
-    <View>
-      <Text>
-        Hello World!
-      </Text>
-    </View>
+    <HomeScreen />
   );
 }
