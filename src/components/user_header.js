e6abd3b2689c48413f85c0a32ae86543d40ff215
gacneves <gabriel.angelo1008@gmail.com>
2022-06-09 23:40:14 -0300

    Separated header layout

--- src/components/user_header.js
+++ src/components/user_header.js
@@ -4,13 +4,23 @@ import { StyleSheet, Text, View } from "react-native";
 export default function UserHeader(props) {
   return (
     <View style={[styles.container, {flex: props.flex_size}]}>
-      <Text>I am User Header from Home Screen!</Text>
+      <View style={{flex:1}}>
+        <Text>User Photo</Text>
+      </View>
+      <View style={{flex:1}}>
+        <Text>User Name</Text>
+      </View>
+      <View style={{flex:1}}>
+        <Text>Chat</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#6D6A75'
   }
 });
