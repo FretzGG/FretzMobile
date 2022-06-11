8278800d4798303aacc053a7cb879fbf2be8a185
gacneves <gabriel.angelo1008@gmail.com>
2022-06-11 18:13:45 -0300

    Added more than one images option and provisional touchables

--- src/scenes/delivery-request-details.js/components/package-photos.js
+++ src/scenes/delivery-request-details.js/components/package-photos.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 export default function PackagePhotos() {
   const [images, setImages] = useState([])
 
-  // TODO: Adicionar galeria de imagens
+  // TODO: Adicionar visualizacao e galeria de imagens
   return (
-    <View>
-      <View style={styles.main_image_preview}>
+    <View style={{flexDirection: 'row'}}>
+      <TouchableOpacity 
+        onPress={() => setImages([...images, 0])}
+        style={styles.main_image_preview}
+      >
         {images.length === 0 ? (
           <View style={{flex: 1, alignItems:'center', justifyContent: 'center'}}>
             <FontAwesomeIcon
@@ -19,12 +22,18 @@ export default function PackagePhotos() {
             />
             <Text style={styles.no_images_text}>SEM IMAGENS</Text>
           </View>
-        ) : images.length === 1 ? (
-          <Text style={styles.no_images_text}>1</Text>
         ) : (
-          <Text style={styles.no_images_text}>2</Text>
+          <Image source={require('../../../assets/package.png')} style={{width: 100, height: 100}}/>
         )}
-      </View>
+      </TouchableOpacity>
+      {images.length > 1 && (
+        <TouchableOpacity
+          onPress={() => setImages([])}
+          style={styles.more_images}
+        >
+          <Text>+{images.length - 1}</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
@@ -39,5 +48,14 @@ const styles = StyleSheet.create({
     color: '#DEB841',
     fontSize: 10,
     fontWeight: 'bold'
+  },
+  more_images: {
+    height: 50,
+    width: 50,
+    backgroundColor: '#E6E6E6',
+    marginTop: 50,
+    marginLeft: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
