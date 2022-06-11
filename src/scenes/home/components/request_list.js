b5b4bf93567ad3de63f7bf47b4f1be0047e705be
gacneves <gabriel.angelo1008@gmail.com>
2022-06-10 22:09:33 -0300

    Added list of requests

--- src/scenes/home/components/request_list.js
+++ src/scenes/home/components/request_list.js
@@ -1,9 +1,31 @@
-import React from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faBoxArchive, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faBoxArchive, faPlus, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function RequestList(props){
+  const [requests, setRequests] = useState([
+    {id: 1, title: 'Requisição 1'},
+    {id: 2, title: 'Requisição 2'},
+    {id: 3, title: 'Requisição 3'}])
+
+  const Item = ({title}) => (
+    <View style={styles.item_box}>
+      <TouchableOpacity onPress={() => {alert('Ver detalhes de ' + title)}} style={styles.item_view}>
+        <Text style={styles.item_text}>
+          {title}
+        </Text>
+        <View style={styles.item_arrow}>
+          <FontAwesomeIcon 
+            icon={faAngleRight}
+            color={'#E6E6E6'}
+            size={30}
+          />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+  
   return (
     <View style={{flex: props.flex_size}}>
       <View style={styles.header}>
@@ -15,16 +37,22 @@ export default function RequestList(props){
           />
           <Text style={styles.request_header_text}>   Minhas Requisições</Text>
         </View>
-        <Pressable onPress={() => {alert('Adicionar requisicao')}} style={styles.request_header_add_view}>
+        <TouchableOpacity onPress={() => {alert('Adicionar requisicao')}} style={styles.request_header_add_view}>
           <FontAwesomeIcon 
             icon={faPlus}
             color={'#DEB841'}
             size={30}
           />
-        </Pressable>
+        </TouchableOpacity>
       </View>
       <View style={styles.list}>
-        <Text>Teste</Text>
+        <FlatList
+          data={requests}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <Item title={item.title} />
+          )}
+        />
       </View>
     </View>
   );
@@ -34,7 +62,7 @@ const styles = StyleSheet.create({
   header: {
     flex: 1,
     flexDirection: 'row',
-    borderBottomWidth: 2,
+    borderBottomWidth: 1,
     borderBottomColor: '#E6E6E6'
   },
   request_header_title_view: {
@@ -54,5 +82,23 @@ const styles = StyleSheet.create({
   },
   list: {
     flex: 7
+  },
+  item_box: {
+    
+    borderBottomWidth: 1,
+    borderBottomColor: '#E6E6E6'
+  },
+  item_view: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 50,
+  },
+  item_text: {
+    color: '#E6E6E6',
+    marginLeft: 20
+  },
+  item_arrow: {
+    marginRight: 25
   }
 });
