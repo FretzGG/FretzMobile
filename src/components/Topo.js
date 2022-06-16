6ecacfaeec22a72847c519391e6a30b12d30c831
gacneves <gabriel.angelo1008@gmail.com>
2022-06-16 13:58:42 -0300

    Added Fretz icon in logo

--- src/components/Topo.js
+++ src/components/Topo.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faTruckFast } from "@fortawesome/free-solid-svg-icons";
 
 export default function Topo() {
   return <View style = {estilos.container}>
     <Text style = {estilos.fonte}>FRETZ</Text>
+    <View style={estilos.icon}>
+      <FontAwesomeIcon icon={faTruckFast} size={42} color={'#DEB841'}/>
+    </View>
   </View>
 }
 
@@ -13,6 +18,7 @@ const estilos = StyleSheet.create({
     backgroundColor: '#37323E',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row'
   },
   fonte: {
     marginTop: 24,
@@ -21,5 +27,9 @@ const estilos = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     color: '#E6E6E6',
+  },
+  icon: {
+    paddingLeft: 5,
+    paddingTop: 15
   }
 });
