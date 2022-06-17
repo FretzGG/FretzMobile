4bc70a731f26b36982af26fe40c6ae33d814634c
gacneves <gabriel.angelo1008@gmail.com>
2022-06-17 10:32:52 -0300

    Refactor: user as a context

--- src/scenes/home/index.js
+++ src/scenes/home/index.js
@@ -1,16 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { StyleSheet, View } from "react-native";
+import { UserContext } from "../../navigators/app-navigator";
 import RequestList from "./components/request-list";
 import UserHeader from "./components/user-header";
 
 export default function HomeScreen() {
-  const [userType, setUserType] = useState(2);
-
-  const user_types = ['ClientePF', 'ClientePJ', 'Motorista']
+  const user = useContext(UserContext);
 
   return (
     <View style={styles.container}>
-      <UserHeader userType={user_types[userType]} flex_size={1}/>
+      <UserHeader userType={user.type} flex_size={1}/>
       <RequestList flex_size={5}/>
     </View>
   );
