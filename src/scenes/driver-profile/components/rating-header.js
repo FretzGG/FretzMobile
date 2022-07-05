a66585204076c610795501f562044d26b03800ca
gacneves <gabriel.angelo1008@gmail.com>
2022-07-04 23:54:54 -0300

    Added profile rating header

--- src/scenes/driver-profile/components/rating-header.js
+++ src/scenes/driver-profile/components/rating-header.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import ProfileIcon from "../../../components/profile-icon";
 
 export default function RatingHeader() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <Text>
-        Header
-      </Text>
+      <TouchableOpacity 
+        style={[styles.center, {flex: 1}]}
+        onPress={() => navigation.goBack()}
+      >
+        <FontAwesomeIcon icon={faArrowLeft} color={'#DEB841'} size={20} />
+      </TouchableOpacity>
+      <View style={{flex: 3, marginTop: 12, alignItems: 'flex-end'}}>
+        <Text style={styles.rating_screen_title}>Avaliações</Text>
+        <Text style={styles.user_name}>Guguinha Martins</Text>
+      </View>
+      <View style={[styles.center, {flex: 2}]}>
+        <ProfileIcon iconSize={60} iconColor={'#DEB841'} circleRadius={85} circleColor={'#37323E'} />
+      </View>
     </View>
   );
 }
@@ -16,5 +32,20 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     backgroundColor: '#6D6A75'
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  rating_screen_title: {
+    color: '#E6E6E6',
+    fontWeight: 'bold',
+    fontSize: 40
+  },
+  user_name: {
+    color: '#E6E6E6',
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginStart: 20
   }
 })
