9540478b43d27893722b9c748f7345b20ff4037c
gacneves <gabriel.angelo1008@gmail.com>
2022-07-18 00:06:45 -0300

    Added chat list item

--- src/scenes/chat-list/components/chat-item.js
+++ src/scenes/chat-list/components/chat-item.js
@@ -1,14 +1,35 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import ProfileIcon from "../../../components/profile-icon";
 
 export default function ChatItem (props) {
   return (
-    <View>
-      <Text>Este é um item com id {props.chat.deliveryID} </Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.item_view}>
+        <ProfileIcon iconSize={30} iconColor={'#37323E'} circleRadius={40} circleColor={'#DEB841'} />
+        <Text style={{color: '#E6E6E6'}} >Pedido #{props.chat.deliveryID}</Text>
+        <FontAwesomeIcon 
+          icon={faAngleRight}
+          color={'#E6E6E6'}
+          size={30}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = {
-
+  container: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#E6E6E6',
+  },
+  item_view: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 50,
+    paddingHorizontal: 10
+  }
 }
