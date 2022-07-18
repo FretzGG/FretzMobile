1a0e5ea911d92db61ac014dcd6dde85bd42f95a0
gacneves <gabriel.angelo1008@gmail.com>
2022-07-18 00:37:45 -0300

    Created message input

--- src/scenes/chat/components/message-input.js
+++ src/scenes/chat/components/message-input.js
@@ -1,16 +1,48 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import LargeButton from "../../../components/large-button";
 
 export default function MessageInput () {
+  const [message, setMessage] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Input</Text>
+      <View style={{marginLeft: 20}}>
+        <Text style={styles.input_title}>Mensagem</Text>
+        <TextInput
+          style={styles.message_input}
+          autoCapitalize={'sentences'}
+          multiline={true}
+          numberOfLines={4}
+          value={message}
+          onChangeText={setMessage}
+        />
+      </View>
+      <LargeButton 
+        title={'Enviar'}
+        onPress={() => {alert('Enviar mensagem')}}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2
+    flex: 2,
+    justifyContent: 'flex-end'
+  },
+  input_title: {
+    color: '#DEB841',
+    fontWeight: 'bold',
+    marginTop: 10
+  },
+  message_input: {
+    backgroundColor: '#E6E6E6',
+    color: '#37323E',
+    marginTop: 10,
+    borderRadius: 30,
+    height: 88,
+    marginRight: 20,
+    marginBottom: -20
   }
 })
