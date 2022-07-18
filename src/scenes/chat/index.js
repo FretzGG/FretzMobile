317ec933a6e81e95fcc4428c576f67f951608156
gacneves <gabriel.angelo1008@gmail.com>
2022-07-18 00:24:09 -0300

    Added profile icon on header for more delivery info

--- src/scenes/chat/index.js
+++ src/scenes/chat/index.js
@@ -1,7 +1,24 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import ProfileIcon from "../../components/profile-icon";
 
 export default function Chat () {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity 
+          onPress={() => {
+            alert('Mostrar informações do FRETZ');
+          }}>
+            <ProfileIcon iconSize={30} iconColor={'#DEB841'} circleRadius={40} circleColor={'#37323E'} />
+        </TouchableOpacity>
+      )
+    })
+  })
+
   return (
     <View style={styles.container}>
       <Text>Chat com fulano</Text>
