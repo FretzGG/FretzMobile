97838fba36f1f35faa92946b379c586b6db3798e
gacneves <gabriel.angelo1008@gmail.com>
2022-07-31 14:04:43 -0300

    Initial chat changes

--- src/scenes/chat-list/components/chat-item.js
+++ src/scenes/chat-list/components/chat-item.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Text, TouchableOpacity, View } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import ProfileIcon from "../../../components/profile-icon";
+import { UserContext } from "../../../navigators/app-navigator";
+import UserIcon from "../../../components/user-icon";
 
 export default function ChatItem (props) {
   const navigation = useNavigation();
+
+  const user = useContext(UserContext);
 
   return (
     <View style={styles.container}>
       <TouchableOpacity 
         style={styles.item_view}
         onPress={() => navigation.navigate('Chat', {
-          title: 'Pedido #' + props.chat.deliveryID
+          title: 'Pedido #' + props.chat.shipping
         })}
       >
         <View style={{flexDirection: 'row'}}>
-          <ProfileIcon iconSize={30} iconColor={'#37323E'} circleRadius={40} circleColor={'#DEB841'} />
+          {/* <UserIcon */}
           <View style={{justifyContent: 'center',  marginLeft: 10}}>
-            <Text style={{color: '#E6E6E6', fontWeight: 'bold'}} >Pedido #{props.chat.deliveryID}</Text>
-            <Text style={{color: '#E6E6E6', fontSize: 10}} >{props.chat.clientName}</Text>
+            <Text style={{color: '#E6E6E6', fontWeight: 'bold'}} >Pedido #{props.chat.shipping}</Text>
+            <Text style={{color: '#E6E6E6', fontSize: 10}} >{user.name !== props.chat.user_one ? props.chat.user_one : props.chat.user_two}</Text>
           </View>
         </View>
         <FontAwesomeIcon 
--- src/scenes/chat-list/index.js
+++ src/scenes/chat-list/index.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
+import { UserContext } from "../../navigators/app-navigator";
+import { AuthContext } from "../../navigators/root-navigator";
 import ChatItem from "./components/chat-item";
 
 export default function ChatList () {
-  const chats = [
-    {id: 0, clientName: 'Guguinha Martins', deliveryID: 1234},
-    {id: 1, clientName: 'Guguinha Lopes', deliveryID: 4567},
-    {id: 2, clientName: 'Guguinha Cardoso', deliveryID: 6910}
-  ];
+  const user = useContext(UserContext);
+  const { userToken } = useContext(AuthContext);
+
+  const [ chats, setChats ] = useState([]);
+
+  useEffect(() => {
+    fetch('http://10.0.2.2:8000/api/chat/get_user_chats/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${userToken}`
+      },
+      body: JSON.stringify({
+        user: user.id
+      })
+    })
+    .then(resp => resp.json())
+    .then(jsonResp => {console.log(jsonResp); setChats(jsonResp)})
+    .catch(error => console.log(error))
+  }, [])
+
+  // const chats = [
+  //   {id: 0, clientName: 'Guguinha Martins', deliveryID: 1234},
+  //   {id: 1, clientName: 'Guguinha Lopes', deliveryID: 4567},
+  //   {id: 2, clientName: 'Guguinha Cardoso', deliveryID: 6910}
+  // ];
 
   return (
     <View style={styles.container}>
